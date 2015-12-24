7fe92e952b00186056719c3f85bcc8d934409d22
azer89 <radhitya@uwaterloo.ca>
2015-12-24 14:15:50 -0500

    added a module

--- droste_effect_2.py
+++ droste_effect_2.py
@@ -14,86 +14,8 @@ import numpy as np
 import matplotlib
 import matplotlib.pylab as plt
 
+import drostelib.drostelib as dl
 
-"""
-Magenta color as the mask color
-"""
-transp_color = np.array([255, 0, 255])
-
-
-"""
-Determine whether a color is the mask color
-"""
-def IsMasked(col):    
-    if(col[0] == transp_color[0] and col[1] == transp_color[1] and col[2] == transp_color[2]):
-        return True
-    return False
-
-
-"""
-Make sure a coordinate is valid
-(or is this because a bug in my program?)
-"""
-def IsCoordValid(x, y):
-    # if x is invalid
-    if(np.isnan(x) or np.isinf(x)):
-        return False            
-    # if y is invalid
-    if(np.isnan(y) or np.isinf(y)):
-        return False
-    # the coordinate is valid
-    return True
-
-
-"""
-make sure a coordinate is inside the image
-"""
-def IsInside(x, y, width, height):
-    if(x >= 0 and y >= 0 and x < width and y < height):
-        return True
-    # is inside
-    return False
-
-
-"""
-Calculate the center of the mask and the approximate radius
-"""
-def CalculateCenter(img_col):
-    r1 = 0.0
-    center_x = 0.0
-    center_y = 0.0
-    pixel_counter = 0.0   
-    
-    xs = [] # list of x-coordinates
-    ys = [] # list of y-coordinates
-    
-    height, width, depth = img_col.shape
-    
-    # sum of the region
-    for y_iter in xrange(height):
-        for x_iter in xrange(width):
-            col = img_col[y_iter][x_iter]
-            if(IsMasked(col)):
-                center_x += x_iter
-                center_y += y_iter
-                xs.append(x_iter)
-                ys.append(y_iter)                
-                pixel_counter += 1.0
-                
-    # get the center
-    center_x /= pixel_counter
-    center_y /= pixel_counter
-                
-    #
-    for i in xrange(len(xs)):
-        x = xs[i] - center_x
-        y = ys[i] - center_y
-        r = np.sqrt(x * x + y * y)
-        if (r > r1):
-            r1 = r
-            
-    # return the radius and the center
-    return r1, center_x, center_y        
     
 
     
@@ -108,7 +30,7 @@ if __name__ == "__main__":
     height_origin, width_origin, depth = img_col.shape    
     img_droste = np.zeros(img_col.shape, dtype="uint8") 
     
-    r1, center_x, center_y = CalculateCenter(img_col)
+    r1, center_x, center_y = dl.CalculateCenter(img_col)
     r2 = center_y if center_y < center_x else center_x
         
     # precompute variables
@@ -127,6 +49,7 @@ if __name__ == "__main__":
     exp_alpha = np.exp(1j * alpha)
         
     for x_iter in xrange(width_origin):
+        
         for y_iter in xrange(height_origin):
             
             xy1 = complex(x_iter - center_x, y_iter - center_y) 
@@ -148,13 +71,13 @@ if __name__ == "__main__":
                 new_y = np.imag(xy4) + center_y
                 
                 # somehow my code produces Nan or Inf (why?)
-                if not (IsCoordValid(new_x, new_y)):
+                if not (dl.IsCoordValid(new_x, new_y)):
                     continue            
                 
                 # the new coordinate should be inside the image
-                if(IsInside(new_x, new_y, width_origin, height_origin)):
+                if(dl.IsInside(new_x, new_y, width_origin, height_origin)):
                     ori_col = img_col[int(new_y)][int(new_x)] 
-                    if not(IsMasked(ori_col)):
+                    if not(dl.IsMasked(ori_col)):
                         img_droste[y_iter][x_iter] = ori_col
                         break
     
---
+++ drostelib/drostelib.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 12:59:31 2015
+
+@author: azer
+"""
+
+import numpy as np
+import matplotlib
+import matplotlib.pylab as plt
+
+
+"""
+Magenta color as the mask color
+"""
+transp_color = np.array([255, 0, 255])
+
+
+"""
+Determine whether a color is the mask color
+"""
+def IsMasked(col):    
+    if(col[0] == transp_color[0] and col[1] == transp_color[1] and col[2] == transp_color[2]):
+        return True
+    return False
+
+
+"""
+Make sure a coordinate is valid
+(or is this because a bug in my program?)
+"""
+def IsCoordValid(x, y):
+    # if x is invalid
+    if(np.isnan(x) or np.isinf(x)):
+        return False            
+    # if y is invalid
+    if(np.isnan(y) or np.isinf(y)):
+        return False
+    # the coordinate is valid
+    return True
+
+
+"""
+make sure a coordinate is inside the image
+"""
+def IsInside(x, y, width, height):
+    if(x >= 0 and y >= 0 and x < width and y < height):
+        return True
+    # is inside
+    return False
+
+
+"""
+Calculate the center of the mask and the approximate radius
+"""
+def CalculateCenter(img_col):
+    r1 = 0.0
+    center_x = 0.0
+    center_y = 0.0
+    pixel_counter = 0.0   
+    
+    xs = [] # list of x-coordinates
+    ys = [] # list of y-coordinates
+    
+    height, width, depth = img_col.shape
+    
+    # sum of the region
+    for y_iter in xrange(height):
+        for x_iter in xrange(width):
+            col = img_col[y_iter][x_iter]
+            if(IsMasked(col)):
+                center_x += x_iter
+                center_y += y_iter
+                xs.append(x_iter)
+                ys.append(y_iter)                
+                pixel_counter += 1.0
+                
+    # get the center
+    center_x /= pixel_counter
+    center_y /= pixel_counter
+                
+    #
+    for i in xrange(len(xs)):
+        x = xs[i] - center_x
+        y = ys[i] - center_y
+        r = np.sqrt(x * x + y * y)
+        if (r > r1):
+            r1 = r
+            
+    # return the radius and the center
+    return r1, center_x, center_y 
